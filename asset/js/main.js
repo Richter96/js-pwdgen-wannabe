629ef74fa9b33379eb0f34a987e97bfa28c5d60a
Richter96 <castiglione96.rc@gmail.com>
2023-03-07 15:55:25 +0100

    add variabili

---
+++ asset/js/main.js
@@ -0,0 +1,27 @@
+
+
+/*
+Decrizione passaggi:
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
+*/
+
+
+// Chiedi all'utente il suo nome,
+let userName = prompt('Inserisci il tuo nome')
+console.log(userName)
+
+// poi chiedi il suo cognome
+let surname=prompt('Inserisci il tuo cognome')
+console.log(surname)
+
+
+// poi chiedi il suo colore preferito
+let userFavouriteColor = prompt('inserisci il tuo colore preferito')
+console.log(userFavouriteColor)
+
+
+//Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
+
