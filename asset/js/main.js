7515d3dda19e7d8d67b9f2dcb5090bd06dafdb1c
Richter96 <castiglione96.rc@gmail.com>
2023-03-07 17:05:00 +0100

    add number of year in secure password edit profile

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,24 +10,33 @@ Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolore
 
 
 // Chiedi all'utente il suo nome,
-const userName = prompt('Inserisci il tuo nome')
+const userName = prompt('Inserisci il tuo nome');
 
 
 // poi chiedi il suo cognome
-const surname = prompt('Inserisci il tuo cognome')
+const surname = prompt('Inserisci il tuo cognome');
 
 
 
 // poi chiedi il suo colore preferito
-const userFavouriteColor = prompt('inserisci il tuo colore preferito')
+const userFavouriteColor = prompt('inserisci il tuo colore preferito');
 
+//aggiunta anno di nascita come numero finale
+const userAge = prompt('inserici la tua età');
+const year = prompt('inserisci anno in corso');
+const birthday = year - userAge;
 
 
 //Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
 /* console.log( `questa è la tua password sicurissima : ${userName}${surname}${userFavouriteColor}23`)
  */
 
-const securePassword = document.getElementById('password')
-console.log(securePassword)
 
-securePassword.innerHTML = `Questa è la tua password sicurissima : ${userName}${surname}${userFavouriteColor}23`
+const securePassword = document.getElementById('password');
+console.log(securePassword);
+
+securePassword.innerHTML = `Questa è la tua password sicurissima : ${userName}${surname}${userFavouriteColor}${birthday}`;
+
+
+
+
