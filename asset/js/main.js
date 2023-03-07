2a36bed53d627a392660a2bf2bf8af6ab7291112
Richter96 <castiglione96.rc@gmail.com>
2023-03-07 16:16:12 +0100

    add consol.log password

--- asset/js/main.js
+++ asset/js/main.js
@@ -11,17 +11,18 @@ Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolore
 
 // Chiedi all'utente il suo nome,
 let userName = prompt('Inserisci il tuo nome')
-console.log(userName)
+
 
 // poi chiedi il suo cognome
 let surname=prompt('Inserisci il tuo cognome')
-console.log(surname)
+
 
 
 // poi chiedi il suo colore preferito
 let userFavouriteColor = prompt('inserisci il tuo colore preferito')
-console.log(userFavouriteColor)
+
 
 
 //Infine scrivi sulla pagina il risultato usando questo formato: nomecognomecolorepreferito23 
+console.log( `questa è la tua password sicurissima : ${userName}${surname}${userFavouriteColor}`)
 
